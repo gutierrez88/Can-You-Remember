691e01045f566e86553373c3baee253ca87339b1
The Real One <gutierrez88@gmail.com>
2019-04-09 17:09:18 -0400

    minor looks and potfolio link

--- src/components/Top/index.js
+++ src/components/Top/index.js
@@ -4,10 +4,15 @@ import "./style.css";
 function Top (props) {
   return (
     <div className="top row z-depth-4 valign-wrapper">
-    <div className="col s6 m3 offset-m1 currentScore">
+    <div className="col s6 m5 push-m1 currentScore">
       Current Score: {props.current}
     </div>
-    <div className="col s5 m4 offset-m3 right-align">
+    <div>
+        <a href="https://gutierrez88.github.io/" className="valign-wrapper">
+            <img className="logoRyan" src="./images/whiteLogo.png" alt="Francisco Gutierrez Logo"></img>
+        </a>
+    </div>
+    <div className="col s6 m5 pull-m1 right-align">
       Best Score: {props.best}
     </div>
   </div>
